b35d229f34e27e04c265035fd33ec844dc5bd452
Blake Netzeband <blake.netzeband@gmail.com>
2016-04-25 09:55:07 -0600

    added bs links

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-angular.module('playgroundApp', [])
-.controller('playgroundController', function($scope) {
+angular.module('pgApp', [])
+.controller('pgController', function($scope) {
   $scope.namechange = function () {
     $scope.greeting = "Hello" + $scope.name;
   };
